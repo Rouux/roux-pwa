f54c18e6cba302fed82d393f7479e0c865cb8e41
Thomas "Roux" Raverdy <thraverdy@gmail.com>
2020-12-10 21:15:57 +0100

    prettier and vue material

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import { MdCard } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+
+Vue.use(MdCard);
 
 Vue.config.productionTip = false;
 
@@ -9,5 +14,5 @@ new Vue({
   router,
   render: function(h) {
     return h(App);
-  }
-}).$mount("#app");
+  },
+}).$mount('#app');
