dab39190d10cdad60f5fe7b144c7bf834fcab1b6
Thomas "Roux" Raverdy <thraverdy@gmail.com>
2020-12-10 21:46:48 +0100

    added card sample

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ title }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -8,6 +8,24 @@
         >vue-cli documentation</a
       >.
     </p>
+    <md-card md-with-hover class="isolate-card">
+      <md-ripple>
+        <md-card-header>
+          <div class="md-title">{{ title }}</div>
+          <div class="md-subhead">It also have a ripple</div>
+        </md-card-header>
+
+        <md-card-content>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque
+          ea, nostrum odio. Dolores, sed accusantium quasi non.
+        </md-card-content>
+
+        <md-card-actions>
+          <md-button>Like</md-button>
+          <md-button>Share</md-button>
+        </md-card-actions>
+      </md-ripple>
+    </md-card>
   </div>
 </template>
 
@@ -15,13 +33,16 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String,
+    title: String,
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.isolate-card {
+  margin: 1em;
+}
 h3 {
   margin: 40px 0 0;
 }
